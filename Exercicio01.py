b97cb9ee04ebfe9f0a2ea3844d721588f0e0ed1f
Jean Goetten <selbstdenkerdot@hotmail.com>
2023-08-03 12:22:40 -0300

    Add files via upload

--- Exercicio01.py
+++ Exercicio01.py
@@ -4,8 +4,8 @@
 # A primeira vale 40% e a segunda vale 60%
 # Elabore um algoritmo que leia as duas notas e mostre a media do estudante
 
-nota01 = int(input("Insira a nota do trabalho: "))
-nota02 = int(input("Insira a nota da prova: "))
+nota01 = float(input("Insira a nota do trabalho: "))
+nota02 = float(input("Insira a nota da prova: "))
 
 media_pond = (nota01*40 + nota02*60)/100
 
