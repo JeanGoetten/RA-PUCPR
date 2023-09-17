db586e918db9869884ebd3c4bc35d7a63481b8e1
Jean Goetten <selbstdenkerdot@hotmail.com>
2023-09-17 19:46:04 -0300

    3 requisitos + 2 funcões

--- Games/JogoDaSenha.py
+++ Games/JogoDaSenha.py
@@ -10,22 +10,87 @@
 # 10) escolha livre
 
 import random
+from string import punctuation
 
 print("================== Jogo da Senha ==================")
 
 password = str()
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-sum = int()
-def pass_analyser(password):
-    for i in range(0, len(password) - 1):
-        for j in range(0, len(numbers) - 1):
-            if password[i] == numbers[j]:
-                sum = sum + int(password[i])
-        
+
+roman_char = ["I", "V", "X", "L", "C", "D", "M"]
+
+def subtitle_progress():
+    subtitle = str(len(password)) + " caracteres"
+    if have_sum(password) > -1:
+        subtitle += " - Número: ok"
+    if is_upper(password):
+        subtitle += " - Maiúscula: ok"
+    if is_special(password):
+        subtitle += " - Char especial: ok"
+    if is_roman(password):
+        subtitle += " - Número romano: ok"
+
+    return subtitle
+
+
+def have_sum(senha_sum):
+    sum_digit = -1
+    for x in senha_sum:
+        if x.isdigit():
+            z = int(x)
+            sum_digit = sum_digit + z
+
+    return sum_digit
+
+
+def is_upper(senha_upper):
+    upper = False
+    for i in senha_upper:
+        if i.isupper():  # verifica maiúsculas
+            upper = True
+        else:
+            upper = False
+
+    return upper
+
+
+def is_special(senha_special):
+    special = False
+    for i in range(0, len(senha_special) - 1):
+        for j in range(0, len(punctuation) - 1):  # verifica se contém caracteres especiais
+            if senha_special[i] == punctuation[j]:
+                special = True
+            else:
+                special = False
+
+        return special
+
+
+def is_roman(senha_roman):
+    roman = False
+    for i in range(0, len(senha_roman) - 1):
+        for j in range(0, len(roman_char) - 1):  # verifica se contém números romanos
+            if senha_roman[i] == roman_char[j]:
+                roman = True
+            else:
+                roman = False
+
+    return roman
 
 
 while True:
-    print(f"Senha: {password }")
+    print(f"Senha: {password} - {subtitle_progress()}")
     if len(password) < 5:
         password = input("Insira uma senha com 5 caracteres: ")
     else:
+        if have_sum(password) < 0:
+            str_number = input("Insira um número positivo: ")
+            if str_number.isnumeric():
+                password += str_number
+        else:
+            if not is_upper(password):
+                str_upper = input("Insira uma letra maiúscula: ")
+                if str_upper.isupper():
+                    password += str_upper
+                else:
+                    print(f"Maiúscula: ok")
+                    break
