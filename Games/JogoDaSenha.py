9b1d1a24be0a9ad6d944d3be9e86084e9c59d944
Jean Goetten <selbstdenkerdot@hotmail.com>
2023-09-18 18:59:10 -0300

    fix special and roman + add partial subtract

--- Games/JogoDaSenha.py
+++ Games/JogoDaSenha.py
@@ -9,6 +9,7 @@
 # 9) escolha livre
 # 10) escolha livre
 
+import random
 print("================== Jogo da Senha ==================")
 
 password = str()
@@ -16,8 +17,9 @@ password = str()
 special_char = ['~', ':', "'", '+', '[', '\\', '@', '^', '{', '%', '(', '-', '"', '*', '|', ',', '&', '<', '`', '}', '.', '_', '=', ']', '!', '>', ';', '?', '#', '$', ')', '/']
 roman_char = ["I", "V", "X", "L", "C", "D", "M"]
 
+message_sum = "Insira digitos para a senha somar 25"
 def subtitle_progress():
-    subtitle = str(len(password)) + " caracteres"
+    subtitle = str(len(password)) + " caracteres" + " - Soma: " + str(have_sum(password, True))
     if is_number(password):
         subtitle += " - Número: ok"
     if is_upper(password):
@@ -26,29 +28,35 @@ def subtitle_progress():
         subtitle += " - Char especial: ok"
     if is_roman(password):
         subtitle += " - Número romano: ok"
-    if have_sum(password) == 25:
-        subtitle += " - Soma: ok"
+        subtitle += " - Número aleatório: ok"
     return subtitle
 
 
 def is_number(senha_number):
-    number = False
     for x in senha_number:
-        if x.isnumeric():
-            number = True
-            break
-        else:
-            number = False
-    return number
-
-
-def have_sum(senha_sum):
-    sum_digit = 0
-    for x in senha_sum:
         if x.isdigit():
+            return True
+
+
+def have_sum(senha_sum, sum):
+    sub_index = 0
+    sum_digit = 0
+    global message_sum
+    global password
+    new_password = password
+    for x in senha_sum:
+        if x.isnumeric():
             z = int(x)
             sum_digit = sum_digit + z
-    print(sum_digit)
+    if not sum:
+        for x in range(0, len(password) - 1):
+            if password[x].isnumeric():
+                if sub_index < len(senha_sum):
+                    print(f"{senha_sum[sub_index]} for {x}")
+                    new_password = password.replace(password[x], senha_sum[sub_index])
+                    print(new_password)
+                    sub_index += 1
+
     return sum_digit
 
 
@@ -65,65 +73,62 @@ def is_upper(senha_upper):
 
 
 def is_special(senha_special):
-    special = False
     for i in senha_special:
         for j in special_char:  # verifica se contém caracteres especiais
             if j == i:
-                special = True
-                break
-            else:
-                special = False
-
-    return special
+                return True
 
 
 def is_roman(senha_roman):
-    roman = False
     for i in senha_roman:
         for j in roman_char:  # verifica se contém números romanos
             if i == j:
-                roman = True
-                break
-            else:
-                roman = False
+                return True
 
-    return roman
+
+def make_random():
+    return random.randint(11, 99)
 
 
 while True:
-    for char in password:
-        print(char)
     print(f"Senha: {password} - {subtitle_progress()}")
     if len(password) < 5:
         password = input("Insira uma senha com 5 caracteres: ")
-    else:
-        if not is_number(password):
-            str_number = input("Insira um número: ")
-            if is_number(str_number):
-                if int(str_number) > -1:
-                    password += str_number
+
+    elif not is_number(password):
+        str_number = input("Insira um número: ")
+        if is_number(str_number):
+            if int(str_number) > -1:
+                password += str_number
+
+    elif not is_upper(password):
+        str_upper = input("Insira uma letra maiúscula: ")
+        if str_upper.isupper():
+            password += str_upper
+
+    elif not is_special(password):
+        str_special = input("Insira um caracter especial: ")
+        if is_special(str_special):
+            password += str_special
+
+    elif have_sum(password, True) != 25:
+        if have_sum(password, True) > 25:
+            str_sum = input(f"Substitua números para a senha somar 25: ")
+            if str_sum.isnumeric():
+                have_sum(str_sum, False)
         else:
-            if not is_upper(password):
-                str_upper = input("Insira uma letra maiúscula: ")
-                if str_upper.isupper():
-                    password += str_upper
-            else:
-                if not is_special(password):
-                    str_special = input("Insira um caracter especial: ")
-                    if is_special(str_special):
-                        password += str_special
-                else:
-                    if not is_roman(password):
-                        str_roman = input("Insira um número romano: ")
-                        if is_roman(str_roman):
-                            password += str_roman
-                    else:
-                        if have_sum(password) != 25:
-                            str_sum = input("Insira digitos para a senha somar 25: ")
-                            if str_sum.isnumeric():
-                                soma_sum = have_sum(str_sum) + have_sum(password)
-                                if soma_sum == 25:
-                                    password += str_sum
-                        else:
-                            break
+            str_sum = input(f"Adicione números para a senha somar 25: ")
+            if str_sum.isnumeric():
+                soma_sum = have_sum(str_sum, True) + have_sum(password, True)
+                if soma_sum == 25:
+                    password += str_sum
+
+    elif not is_roman(password):
+        str_roman = input("Insira um número romano: ")
+        if is_roman(str_roman):
+            password += str_roman
+            password += str(random.randint(11, 99))
+
+    else:
+        break
 
