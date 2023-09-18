d49f9cfa8e71cdc3ee378fa9bd9bbe030dbdc554
Jean Goetten <selbstdenkerdot@hotmail.com>
2023-09-18 00:18:07 -0300

    reqquisitos parciais
    erros em especiais e romanos (verifica falso na segunda iteração, mesmo após ter verificado verdadeiro na primeira)

--- Games/JogoDaSenha.py
+++ Games/JogoDaSenha.py
@@ -9,18 +9,16 @@
 # 9) escolha livre
 # 10) escolha livre
 
-import random
-from string import punctuation
-
 print("================== Jogo da Senha ==================")
 
 password = str()
 
+special_char = ['~', ':', "'", '+', '[', '\\', '@', '^', '{', '%', '(', '-', '"', '*', '|', ',', '&', '<', '`', '}', '.', '_', '=', ']', '!', '>', ';', '?', '#', '$', ')', '/']
 roman_char = ["I", "V", "X", "L", "C", "D", "M"]
 
 def subtitle_progress():
     subtitle = str(len(password)) + " caracteres"
-    if have_sum(password) > -1:
+    if is_number(password):
         subtitle += " - Número: ok"
     if is_upper(password):
         subtitle += " - Maiúscula: ok"
@@ -28,17 +26,29 @@ def subtitle_progress():
         subtitle += " - Char especial: ok"
     if is_roman(password):
         subtitle += " - Número romano: ok"
-
+    if have_sum(password) == 25:
+        subtitle += " - Soma: ok"
     return subtitle
 
 
+def is_number(senha_number):
+    number = False
+    for x in senha_number:
+        if x.isnumeric():
+            number = True
+            break
+        else:
+            number = False
+    return number
+
+
 def have_sum(senha_sum):
-    sum_digit = -1
+    sum_digit = 0
     for x in senha_sum:
         if x.isdigit():
             z = int(x)
             sum_digit = sum_digit + z
-
+    print(sum_digit)
     return sum_digit
 
 
@@ -47,6 +57,7 @@ def is_upper(senha_upper):
     for i in senha_upper:
         if i.isupper():  # verifica maiúsculas
             upper = True
+            break
         else:
             upper = False
 
@@ -55,22 +66,24 @@ def is_upper(senha_upper):
 
 def is_special(senha_special):
     special = False
-    for i in range(0, len(senha_special) - 1):
-        for j in range(0, len(punctuation) - 1):  # verifica se contém caracteres especiais
-            if senha_special[i] == punctuation[j]:
+    for i in senha_special:
+        for j in special_char:  # verifica se contém caracteres especiais
+            if j == i:
                 special = True
+                break
             else:
                 special = False
 
-        return special
+    return special
 
 
 def is_roman(senha_roman):
     roman = False
-    for i in range(0, len(senha_roman) - 1):
-        for j in range(0, len(roman_char) - 1):  # verifica se contém números romanos
-            if senha_roman[i] == roman_char[j]:
+    for i in senha_roman:
+        for j in roman_char:  # verifica se contém números romanos
+            if i == j:
                 roman = True
+                break
             else:
                 roman = False
 
@@ -78,19 +91,39 @@ def is_roman(senha_roman):
 
 
 while True:
+    for char in password:
+        print(char)
     print(f"Senha: {password} - {subtitle_progress()}")
     if len(password) < 5:
         password = input("Insira uma senha com 5 caracteres: ")
     else:
-        if have_sum(password) < 0:
-            str_number = input("Insira um número positivo: ")
-            if str_number.isnumeric():
-                password += str_number
+        if not is_number(password):
+            str_number = input("Insira um número: ")
+            if is_number(str_number):
+                if int(str_number) > -1:
+                    password += str_number
         else:
             if not is_upper(password):
                 str_upper = input("Insira uma letra maiúscula: ")
                 if str_upper.isupper():
                     password += str_upper
+            else:
+                if not is_special(password):
+                    str_special = input("Insira um caracter especial: ")
+                    if is_special(str_special):
+                        password += str_special
                 else:
-                    print(f"Maiúscula: ok")
-                    break
+                    if not is_roman(password):
+                        str_roman = input("Insira um número romano: ")
+                        if is_roman(str_roman):
+                            password += str_roman
+                    else:
+                        if have_sum(password) != 25:
+                            str_sum = input("Insira digitos para a senha somar 25: ")
+                            if str_sum.isnumeric():
+                                soma_sum = have_sum(str_sum) + have_sum(password)
+                                if soma_sum == 25:
+                                    password += str_sum
+                        else:
+                            break
+
